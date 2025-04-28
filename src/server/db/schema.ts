7153c39e947484f61fa3d666cb4d7a9f2703660d
Caio Lins <clins@ksmartsolutions.jp>
2025-04-28 15:05:54 +1000

    Implement users table in database schema and add migration scripts

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,12 +1,9 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-// TODO: delete-later
-export const entriesTable = pgTable("entries", {
-  id: serial("id").primaryKey(),
-  content: text("content").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  email: text("email").notNull().unique(),
+  name: text("name"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
-
-export type Entry = typeof entriesTable.$inferSelect;
-export type NewEntry = typeof entriesTable.$inferInsert;
